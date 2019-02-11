81639b605df001d5c97d02cda88bfb2de6f8ef13
Kirill Opekishev <grumpykir@gmail.com>
2019-02-11 17:11:17 +0300

    Initial Commit

---
+++ GKNetwork/GKNetwork.h
@@ -0,0 +1,19 @@
+//
+//  GKNetwork.h
+//  GKNetwork
+//
+//  Created by  Кирилл on 2/11/19.
+//  Copyright © 2019 AppCraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GKNetwork.
+FOUNDATION_EXPORT double GKNetworkVersionNumber;
+
+//! Project version string for GKNetwork.
+FOUNDATION_EXPORT const unsigned char GKNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GKNetwork/PublicHeader.h>
+
+
